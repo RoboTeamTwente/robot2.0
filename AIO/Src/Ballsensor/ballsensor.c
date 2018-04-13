fac94911ce634bfe8fa72d06ee367a645184e1fa
Unknown <grenblot@gmail.com>
2018-04-13 09:30:48 +0200

    ballsensor: increased scanning frequency & idle frequency + only react to single objects/touches

--- AIO/Src/Ballsensor/ballsensor.c
+++ AIO/Src/Ballsensor/ballsensor.c
@@ -12,12 +12,22 @@ enum zForceStates{
 	zForce_WaitForDR,
 	zForce_DecodeMessage,
 	zForce_ReadMessage,
-	zForce_EnableDevice
+	zForce_EnableDevice,
+	zForce_setFreq
 }zForceState = zForce_RST;
 
 uint8_t enable_command[] = 	{0xEE,0x0B,	0xEE,0x09,0x40,0x02,0x02,0x00,0x65,0x03,0x81,0x01,0x00};
 uint8_t enable_response[] = {			0xEF,0x09,0x40,0x02,0x02,0x00,0x65,0x03,0x81,0x01,0x00};
 uint8_t bootcomplete_response[] = {0xF0, 0x11, 0x40, 0x02, 0x00, 0x00, 0x63, 0x0B, 0x80, 0x01, 0x00, 0x81, 0x02, 0x03, 0x00, 0x82, 0x02, 0x00, 0x00};
+
+//from datasheet, not used atm
+uint8_t config_command[] = {0xEE, 0x1E, 0xEE, 0x1A, 0x40, 0x02, 0x02, 0x00, 0x73, 0x14, 0xA2, 0x12, 0x80, 0x02, 0x00, 0xB5, 0x81, 0x01, 0x43, 0x82, 0x02, 0x06, 0x98, 0x83, 0x02, 0x04, 0x34, 0x85, 0x01, 0xFF};
+
+//set scanning frequency to 900Hz (0x384) & idle frequency to 125 Hz (0x7D)
+uint8_t set_freq_command[] = {0xEE,0x0F,0xEE,0x0D,0x40,0x02,0x00,0x00,0x68,0x07,0x80,0x02,0x03,0x84,0x82,0x01,0x7D};
+uint8_t set_freq_response[] = {0xEF,0x0D,0x40,0x02,0x00,0x00,0x68,0x07,0x80,0x02};
+
+uint8_t measurement_rx[] = {0xf0, 0x11, 0x40, 0x02, 0x02};
 
 uint8_t next_message_length = 2;
 
@@ -33,31 +43,56 @@ void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *I2cHandle)
 
 void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *I2cHandle)
 {
-	if(zForceState == zForce_EnableDevice) {
-			zForceState = zForce_WaitForDR;
-	}
+	zForceState = zForce_WaitForDR;
+}
+
+void printRawData(uint8_t data[]) {
+    uprintf("data = [");
+    for(uint i = 0; i < next_message_length; i++){
+      uprintf("%02x ", data[i]);
+    }
+    uprintf("]\n\r");
+}
+
+void printPosition(uint8_t data[]) {
+  uint16_t x;
+  x = data[12] << 8;
+  x |= data[13];
+  uint16_t y;
+  y = data[14] << 8;
+  y |= data[15];
+  uint16_t ball_position = x;
+  uprintf("BALLSENSOR - x:\t %d \t y:\t %d \n\r", ball_position,y);
+  ballHandler(x,y);
 }
 
 
-void parseMessage() {
-	uint16_t x;
-	x = data[12] << 8;
-	x |= data[13];
-	uint16_t y;
-	y = data[14] << 8;
-	y |= data[15];
-	uprintf("BALLSENSOR - x:\t %d \t y:\t %d \n\r", x,y);
+void ballHandler(uint16_t x, uint16_t y) {
 	kick_Kick(60);
+}
+
+void parseMessage() {
+
 
 	if(!memcmp( data, bootcomplete_response, sizeof(bootcomplete_response))) {
 	  //uprintf("BootComplete response received, enabling device\n\r");
 	  zForceState = zForce_EnableDevice;
 	}
 	else if(!memcmp(data, enable_response, sizeof(enable_response))) {
-		//uprintf("Enable response received, going to waitfordr\n\r");
+		uprintf("Enable response received, going to waitfordr\n\r");
+		zForceState = zForce_setFreq;
+	}
+	else if(!memcmp(data,measurement_rx, sizeof(measurement_rx))) {
+		printPosition(data);
+		zForceState = zForce_WaitForDR;
+	}
+	else if(!memcmp(data,set_freq_response, sizeof(set_freq_response))) {
+		uprintf("Set frequency:\r\n");
+		printRawData(data);
 		zForceState = zForce_WaitForDR;
 	}
 	else {
+		printRawData(data);
 	  zForceState = zForce_WaitForDR;
 	  //uprintf("going to waitfordr\n\r");
 	}
@@ -120,6 +155,15 @@ void ballsensorMeasurementLoop()
 			  }
 
 			  break;
+		  case zForce_setFreq:
+		          uprintf("Setting frequency\n\r");
+		          while(HAL_OK != (error = HAL_I2C_Master_Transmit_IT(&hi2c1, ballsensor_i2caddr, set_freq_command, sizeof(set_freq_command)))){// in case of error; put the device in reset
+		        	  HAL_GPIO_WritePin(bs_nRST_GPIO_Port, bs_nRST_Pin, 0);
+		              uprintf("BALLSENSOR - i2c transmit failed with error [%d]!\n\rzForce stopped\n\r", error);
+		              zForceState = zForce_RST;
+		          }
+
+		      break;
 			  break;
 		  }
 
