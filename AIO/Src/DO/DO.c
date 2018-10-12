147b106555839b6d28e1f764600b37cf9dcd2e92
K.J.Hertenberg <k.j.hertenberg@gmail.com>
2018-10-12 10:01:53 +0200

    Removed functions uncalled due to if statement being always true

--- AIO/Src/DO/DO.c
+++ AIO/Src/DO/DO.c
@@ -125,111 +125,6 @@ float compute_limit_scale(float input[3], float limit){
 	return scale;
 }
 
-void disturbanceObserver(float yaw, float localInput[3], float globalAcc[2], float output[3]){
-	//TODO: this could also be used if no velocity control is used
-	//TODO: add rotational term as well?
-
-	// Requires transformation to global coordinates and back
-	float globalInput[3];
-	rotate(-yaw, localInput, globalInput);
-	// accelerations are already global (free acceleration)
-	float accX = globalAcc[body_x];
-	float accY = globalAcc[body_y];
-
-	float globalOut[3] = {0,0,0};
-	static float prevOut[3] = {0,0,0};
-	if (!isnan(prevOut[0])){ // lowpass filtering
-		globalOut[body_x] = 0.07*(accX*500 - globalInput[body_x]) + 0.93*prevOut[body_x];
-		globalOut[body_y] = 0.07*(accY*500 - globalInput[body_y]) + 0.93*prevOut[body_y];
-	}
-	prevOut[body_x] = globalOut[body_x];
-	prevOut[body_y] = globalOut[body_y];
-
-	// rotate back to local and fill in the output
-	//TODO: differentiate for strafing direction?
-	float out[3];
-	rotate(yaw, globalOut, out);
-	output[body_x] = out[body_x]*0.5; // reduce effect of DO for now for safety
-	output[body_y] = out[body_y]*0.5;
-	output[body_w] = 0;
-}
-
-void pController(float input[3], float kp[3], float output[3]){
-	// These limits are meant to prevent slipping
-	float w_limit = 500;
-	float PWM_limit = 100;
-
-	float pre_out[3];
-	pre_out[body_x] = kp[body_x]*input[body_x];
-	pre_out[body_y] = kp[body_y]*input[body_y];
-	pre_out[body_w] = kp[body_w]*input[body_w];
-
-	float scale = compute_limit_scale(pre_out, PWM_limit);
-
-	if (pre_out[body_w] > w_limit) {
-		pre_out[body_w] = w_limit;
-	} else if (pre_out[body_w] < -w_limit) {
-		pre_out[body_w] = -w_limit;
-	}
-
-	output[body_x] = pre_out[body_x] * scale;
-	output[body_y] = pre_out[body_y] * scale;
-	output[body_w] = pre_out[body_w];
-}
-
-//observer output should just be pre-declared as 0 before any looping starts
-void controller(float localVelocityRef[3], float w_wheels[4], float xsensData[3], float output[4]){
-	static float do_output[3] = {0};
-
-	// Compute the error in local body coordinates
-	float localVel[3];
-	wheels2Body(w_wheels, localVel);
-	float error[3];
-	error[body_x] = localVelocityRef[body_x] - localVel[body_x];
-	error[body_y] = localVelocityRef[body_y] - localVel[body_y];
-	error[body_w] = localVelocityRef[body_w] - localVel[body_w];
-	//	uprintf("[%f, %f, %f]\n\r", localVel[body_x], localVel[body_y],  localVel[body_w]);
-	//	uprintf("[%f %f %f]\n\r", xsensData[body_x], xsensData[body_y], xsensData[body_w]);
-		//uprintf("[%f, %f, %f]\n\r", localReference[body_x], localReference[body_y],  localReference[body_w]);
-
-	// P-control
-	float controllerGain[3];
-	controllerGain[body_x] = 20000/10;
-	controllerGain[body_y] = 20000/10;
-	controllerGain[body_w] = 200/10;
-
-	float pOut[3];
-	pController(error, controllerGain, pOut);
-
-	// Apply the Disturbance Observer output
-	float postObserverSignal[3];
-	postObserverSignal[body_x] = pOut[body_x] - do_output[body_x];
-	postObserverSignal[body_y] = pOut[body_y] - do_output[body_y];
-	postObserverSignal[body_w] = pOut[body_w] - do_output[body_w];
-
-	// Limiting the output to prevent saturation of the PWM signals for any of the wheels
-	float scaledInput[3];
-	float scale = compute_limit_scale(postObserverSignal, 95);
-	scaledInput[body_x] = scale*postObserverSignal[body_x];
-	scaledInput[body_y] = scale*postObserverSignal[body_y];
-	scaledInput[body_w] = 1*postObserverSignal[body_w];
-
-	// Output the wheel PWMs (by filling in the output array)
-	body2Wheels(scaledInput, output);
-
-	// To make sure the robot does not do anything due to the DO when wheels are stationary and acceleration is measured
-	float accData[2] = {xsensData[body_x],xsensData[body_y]};
-	if (sqrt(localVel[body_x]*localVel[body_x] + localVel[body_y]*localVel[body_y])<0.05) {
-		accData[body_x] = 0;
-		accData[body_y] = 0;
-	}
-	// Compute disturbance observer output for next iteration, filling in the DO array (do_output)
-	if (DO_enabled) {
-		disturbanceObserver(xsensData[body_w], scaledInput, accData, do_output);
-	}
-
-}
-
 float angleController(float angleRef, float yaw){
 	// TODO: FEW TWEAKS MADE THAT NEED TESTING
 	// PD Control of the yaw
@@ -290,8 +185,7 @@ void rotVelCompensation(float yaw, float localVelocityRef[3]){
 	rotate(compensation_dir, localVelocityRef, localVelocityRef);
 }
 
-
-void actualControl(float velocityRef[3], float xsensData[3], float w_wheels[4], float output[4]){
+void notControl(float velocityRef[3], float xsensData[3], float w_wheels[4], float output[4]){
 	// determine local velocity reference
 		float localVelocityRef[3] = {velocityRef[body_x],velocityRef[body_y],velocityRef[body_w]};
 		if (use_global_ref) {
@@ -301,40 +195,16 @@ void actualControl(float velocityRef[3], float xsensData[3], float w_wheels[4], 
 		rotVelCompensation(xsensData[body_w], localVelocityRef);
 
 		// yaw  and velocity controllers
-		if (use_yaw_control) {
-			float angleRef;
-			if (ref_is_angle) { //TODO the joystick/software could be used here to directly set the angle reference
-				angleRef = localVelocityRef[body_w] / 16;
-			} else { // for the keyboard the angle reference is ramped up by integrating the signal
-				static float prevAngleRef = 0;
-				angleRef = prevAngleRef + TIME_DIFF * localVelocityRef[body_w];
-				prevAngleRef = angleRef;
-			}
-				if (no_vel_control) {
-				float forceRef[3] = {localVelocityRef[body_x]*1500, localVelocityRef[body_y]*2000, 0};
-				forceRef[body_w] = angleController(angleRef, xsensData[body_w])*2.0F;
-				float scale = compute_limit_scale(forceRef, 100);
-				forceRef[body_x] = scale*forceRef[body_x];
-				forceRef[body_y] = scale*forceRef[body_y];
-				forceRef[body_w] = forceRef[body_w]/2.0F;
-				body2Wheels(forceRef, output);
-			} else {
-				float newVelocityRef[3] = {localVelocityRef[body_x], localVelocityRef[body_y], 0};
-				newVelocityRef[body_w] = angleController(angleRef, xsensData[body_w]);
-				controller(newVelocityRef, w_wheels, xsensData, output);
-			}
-			} else { // no yaw control
-			if (no_vel_control) {
-				float forceRef[3] = {localVelocityRef[body_x]*1000, localVelocityRef[body_y]*1500, localVelocityRef[body_w]*10};
-				float scale = compute_limit_scale(forceRef, 90);
-				forceRef[body_x] = scale*forceRef[body_x];
-				forceRef[body_y] = scale*forceRef[body_y];
-				forceRef[body_w] = 1*forceRef[body_w];
-				body2Wheels(forceRef,output);
-			} else {
-				controller(localVelocityRef, w_wheels, xsensData, output);
-			}
-		}
+		float angleRef;
+		angleRef = localVelocityRef[body_w] / 16;
+		float forceRef[3] = {localVelocityRef[body_x]*1500, localVelocityRef[body_y]*2000, 0};
+		forceRef[body_w] = angleController(angleRef, xsensData[body_w])*2.0F;
+		float scale = compute_limit_scale(forceRef, 100);
+		forceRef[body_x] = scale*forceRef[body_x];
+		forceRef[body_y] = scale*forceRef[body_y];
+		forceRef[body_w] = forceRef[body_w]/2.0F;
+		body2Wheels(forceRef, output);
+
 	return;
 }
 
@@ -445,7 +315,7 @@ bool DO_Control(float velocityRef[3], float vision_yaw, bool vision_available, f
 	// control part
 
 	if (!calibration_needed){
-	actualControl(velocityRef, xsensData, w_wheels, output);
+	notControl(velocityRef, xsensData, w_wheels, output);
 	}
 
 	return calibration_needed;
