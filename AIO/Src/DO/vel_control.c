a1e0c13e2e94b4ef8534b2982426883f50e3a78b
simenbootsma <s.t.bootsma@student.utwente.nl>
2019-01-28 15:05:05 +0100

    Remove velocity control

--- AIO/Src/DO/vel_control.c
+++ AIO/Src/DO/vel_control.c
@@ -22,11 +22,6 @@ void vel_control_Callback(float wheel_ref[4], float State[3], float vel_ref[3]){
 	float angleComp = PID(angleErr, &angleK);// PID control from control_util.h
 	static float velLocalRef[3] = {0};
 	global2Local(vel_ref, velLocalRef, State[body_w]); //transfer global to local
-
-	// PID control from control_util.h
-	velLocalRef[body_x] += PID((velLocalRef[body_x]-State[body_x]), &velxK); //error compensation plus requested velocity
-	velLocalRef[body_y] += PID((velLocalRef[body_y]-State[body_y]), &velyK);
-
 	body2Wheels(wheel_ref, velLocalRef); //translate velocity to wheel speed
 
 	for (int i = 0; i < 4; ++i){
