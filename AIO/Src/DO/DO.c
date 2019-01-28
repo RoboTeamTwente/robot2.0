4cf76000f85b92c9a2b55de228bf3f491e23a346
simenbootsma <s.t.bootsma@student.utwente.nl>
2019-01-28 15:01:53 +0100

    Remove scaling and limiting

--- AIO/Src/DO/DO.c
+++ AIO/Src/DO/DO.c
@@ -20,8 +20,6 @@ static float xsensData[3];
 
 ///////////////////////////////////////////////////// PRIVATE FUNCTION DECLARATIONS
 
-static void scaleAndLimit(float wheel_ref[4]); //scales and limit the signal
-//static void wheelFilter(float w_wheels[4]);
 static void getXsensData(float xsensData[3]);
 
 ///////////////////////////////////////////////////// PUBLIC FUNCTION IMPLEMENTATIONS
@@ -34,10 +32,6 @@ int vel_control_Init(){
 }
 
 void DO_Control(float velocityRef[3], float vision_yaw, bool vision_available, float wheel_ref[4]){
-	// get and filter wheel speeds
-//	float w_wheels[4] = {0,0,0,0};
-//	wheelFilter(w_wheels);//edits the above array
-
 	// get and offset xsens data
 	getXsensData(xsensData);
 
@@ -49,9 +43,6 @@ void DO_Control(float velocityRef[3], float vision_yaw, bool vision_available, f
 
 	// control part
 	vel_control_Callback(wheel_ref, State, velocityRef);
-
-	// limit and scale wheel outputs
-	scaleAndLimit(wheel_ref);
 }
 
 float getYaw() {
@@ -64,21 +55,4 @@ static void getXsensData(float xsensData[3]){
 	xsensData[body_x] = -MT_GetAcceleration()[0]; // TODO: why the minus signs?
 	xsensData[body_y] = -MT_GetAcceleration()[1];
 	xsensData[body_w] = MT_GetAngles()[2]/180*M_PI;
-}
-
-static void scaleAndLimit(float wheel_ref[4]){
-	// TODO: it doesn't work if you use the defined constants from control_util, go fix!
-	float margin = 0.3; // give the PID a little space to adjust
-	float rpsFactor = OMEGAtoPWM;
-	float rps_limit = PWM_LIMIT/rpsFactor;
-	float rps_cutoff = PWM_CUTOFF/rpsFactor + margin;
-	for (wheel_names i = wheels_RF; i <= wheels_LF; i++) {
-		if (fabs(wheel_ref[i]) > rps_limit) {
-			wheel_ref[i] = rps_limit*fabs(wheel_ref[i])/wheel_ref[i];
-		} else if (fabs(wheel_ref[i]) < margin) {
-			wheel_ref[i] = 0;
-		} else if (fabs(wheel_ref[i]) < rps_cutoff) {
-			wheel_ref[i] = rps_cutoff*fabs(wheel_ref[i])/wheel_ref[i];
-		}
-	}
 }
