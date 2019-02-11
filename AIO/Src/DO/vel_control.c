de57699257b38183d0da27782080a266e0ca7234
simenbootsma <s.t.bootsma@student.utwente.nl>
2019-02-11 17:17:31 +0100

    Turn off translational control

--- AIO/Src/DO/vel_control.c
+++ AIO/Src/DO/vel_control.c
@@ -50,10 +50,10 @@ void vel_control_Callback(float wheel_ref[4], float State[3], float vel_ref[3]){
 	float velLocalRef[3] = {0};
 	global2Local(vel_ref, velLocalRef, State[body_w]); //transfer global to local
 
-	float velxErr = velLocalRef[body_x] - State[body_x];
-	float velyErr = velLocalRef[body_y] - State[body_y];
-	velLocalRef[body_x] += PID(velxErr, &velxK);
-	velLocalRef[body_y] += PID(velyErr, &velyK);
+//	float velxErr = velLocalRef[body_x] - State[body_x];
+//	float velyErr = velLocalRef[body_y] - State[body_y];
+//	velLocalRef[body_x] += PID(velxErr, &velxK);
+//	velLocalRef[body_y] += PID(velyErr, &velyK);
 
 	body2Wheels(wheel_ref, velLocalRef); //translate velocity to wheel speed
 
